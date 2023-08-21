7c1ef404e9beb271d8692ebbdfc3ee207b0ab142
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-08-21 23:07:09 +0530

    imageBazzar

---
+++ src/Components/ImageGallery.js
@@ -0,0 +1,19 @@
+
+
+const ImageGallery = ({images}) => {
+
+    return(
+        <div>
+               {
+                images.map( (image) =>(
+                    <img 
+                       src={image.urls.thumb}
+                       alt={image.alt_description}
+                    />
+                ))
+               }
+        </div>
+    )
+}
+
+export default ImageGallery
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,59 @@
+// KeyUp, KeyDown
+import React, {useState, useEffect} from "react"
+import axios from "axios"
+
+const SearchBar = ({setImages}) => {
+   const [searchTerm, setSearchTerm] = useState("nature")
+   const [previousSearchTerm, setPreviousSearchTerm] = useState("")
+
+   console.log(searchTerm)
+  
+
+//    console.log(images)
+
+useEffect(()=>{
+    fetchImage()
+},[]
+)
+
+
+   const fetchImage = () => {
+         if(searchTerm === previousSearchTerm) return
+                axios.get("https://api.unsplash.com/search/photos", {
+                    params:{
+                        // client_id : "3izN2i3fcImpmaZNGgVf4F4ScL_5ckerUZyTU2WlvAU",
+                        query : searchTerm || "nature"
+                    },
+                    headers:{
+                        Authorization: "Client-ID 3izN2i3fcImpmaZNGgVf4F4ScL_5ckerUZyTU2WlvAU"
+                    }
+
+                })
+                    .then((response) => {
+                        setImages(response.data.results)
+                        // setSearchTerm("")
+                        setPreviousSearchTerm(searchTerm)
+                    })
+                    .catch((error) => console.log(error))
+          
+   }
+
+//    function handleKeys(e){
+//     //    console.log(e.key)
+//     if(e.key == "s"){
+//         fetchImage()
+//     }
+//    }
+
+
+    return(
+        <div onKeyDown={handleKeys}>
+            <input type="text" placeholder="Search for an image..." 
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button onClick={fetchImage}>Search</button>
+        </div>
+    )
+}
+
+export default SearchBar
